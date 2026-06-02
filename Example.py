972d46d71cf595e594a6cb2ca6312f2bef80d89f
Mr.Vossoughi <mr.vosooghi.co@gmail.com>
2026-06-02 20:30:29 +0330

    first commit

---
+++ Example.py
@@ -0,0 +1,87 @@
+# Example 01
+# message = 'hello world !'
+# print(message)
+
+
+# Example 02
+"""name= "mohammadreza"
+family = 'vossoughi'
+age = 37
+ave= 18.35
+x = True
+y = False
+print(name+" "+family+" "+str(age) + " years old ")
+print(f"{name} {family} {age} years old")"""
+
+
+# Example 03
+"""name = input('pls enter your name  :')
+family = input('pls enter your lastname ;')
+age  = int(input('pls enter your age ; '))
+fullname = name+" "+family +" "+ str(age)
+print(fullname)"""
+
+# Example 04
+"""name = input('pls enter your name  :')
+family = input('pls enter your lastname ;')
+age  = int(input('pls enter your age ; '))
+fullname = name+" "+family +" "+ str(age)
+print(fullname)
+if age > 18:
+    print("yes")
+else:
+    print("no")"""
+
+
+# Example 05
+# number = int(input("pls enter number  :"))
+# if number > 0:
+#     print("posetive ! ")
+# elif (number < 0):
+#     print("negative !")
+# if(number == 0):
+#     print("zero")
+
+
+# Example 06
+"""txt  = input("pls enter  text : ")
+for item in range(5):
+    print(txt)
+"""
+
+
+# Example 07
+"""for i in range(1,11):
+    print(i)"""
+
+
+# Example 08
+"""def get(txt):
+    print(txt)
+
+
+txt = "hello world !"
+get(txt)"""
+
+
+# Example 09
+"""def mult(number):
+   return number*2
+
+number = int(input('pls enter number :'))
+result = mult(number)
+print(result)
+"""
+
+# Example 10
+"""def proc(x):
+    if x % 2 ==0:
+        print("number is even")
+    else:
+        print("number is Odd")
+number = int(input("pls enter number "))
+proc(number)"""
+
+
+
+
